1deda3c20603fab7a7706043a6a311253bc2211a
Vladimir Leveryev <volodya.leveryev@gmail.com>
2026-06-08 18:59:23 +0900

    ADd media storage settings

--- project/settings.py
+++ project/settings.py
@@ -133,3 +133,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
--- webui/models.py
+++ webui/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class EducationPlan(models.Model):
     name = models.CharField('название', max_length=64)
-    file = models.FileField('файл')
+    file = models.FileField('файл', upload_to='edu_plan/')
 
     class Meta:
         verbose_name = 'учебный план'
